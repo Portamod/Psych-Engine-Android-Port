f78f2f8d4becbf8d7db1c2f67752205543581851
Portamod <95686080+Portamod@users.noreply.github.com>
2021-12-08 07:40:29 +0700

    Charting State.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -58,8 +58,8 @@ class ChartingState extends MusicBeatState
 		'Hey!',
 		'Hurt Note',
 		'GF Sing',
-		'No Animation'
-	];
+		'Bullet-note', 
+	     ];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
 
